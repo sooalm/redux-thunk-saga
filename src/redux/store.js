705b2f851fb445e2c7849a16d54bfd71ce9d0545
Eugene <sshabrovivivi@gmail.com>
2025-10-02 20:06:12 +0400

    saga start

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
-import {configureStore, Tuple} from '@reduxjs/toolkit';
-import { logger } from './middleware/logger';
-import taskReducer from './slices/taskSlice';
+import { configureStore, Tuple } from "@reduxjs/toolkit";
+import { logger } from "./middleware/logger";
+import taskReducer from "./slices/taskSlice";
 
- const store = configureStore({
+const store = configureStore({
   reducer: {
     tasks: taskReducer,
   },
-  middleware:  (getDefaultMiddleware) => 
-  new Tuple(...getDefaultMiddleware(), logger),
+  middleware: (getDefaultMiddleware) =>
+    new Tuple(...getDefaultMiddleware(), logger),
 });
 
 export default store;
